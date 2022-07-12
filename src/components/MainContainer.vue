1bbf481607d69b5e43365b3cd14ec74f7e64c8d6
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-07-12 18:12:03 +0200

    milestone 2 done

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -4,7 +4,7 @@
             <li v-for="movie in movies" :key="movie.id">
                 {{ movie.title }}
                 {{ movie.original_title }}
-                {{ movie.original_language }}
+                <img :src="languageImgUrl + enIsNotANation(movie.original_language)" :alt="`Original language: ${language[movie.original_language]}`">
                 {{ movie.vote_average }}
             </li>
         </ul>
@@ -12,22 +12,33 @@
 </template>
 
 <script>
+import languagesList from "../assets/languages.json";
+
 export default {
     name: "MainContainer",
     data() {
         return {
-             
+            language: languagesList,
+            languageImgUrl: "https://countryflagsapi.com/svg/",
         }
     },
     props: {
         movies: Array
     },
     methods: {
-        
+        enIsNotANation(x) {
+            if( x === "en") {
+                return "gb"
+            } else {
+                return x
+            }
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    img {
+        width: 1rem;
+    }
 </style>
